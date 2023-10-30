d29603c4c846eab0b441f97f2565a0d0fd5b4169
Anurag Kallurwar <anurag.kallurwar@gmail.com>
2023-10-29 23:06:34 -0400

    Input environment

---
+++ Website/code/environment.py
@@ -0,0 +1,21 @@
+"""
+file: environment.py
+description: This program create random obstacles(polygons) based on given
+configuration settings
+language: python3
+author: Anurag Kallurwar, ak6491@rit.edu
+"""
+
+import sys
+
+
+def main():
+    """
+    The main function
+    :return: None
+    """
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()  # Calling Main Function
