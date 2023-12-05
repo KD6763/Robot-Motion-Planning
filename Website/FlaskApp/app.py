c59cb46d4c854a751aa462063828be7452f66f06
Anurag Kallurwar <anurag.kallurwar@gmail.com>
2023-12-04 21:40:53 -0500

    Test

---
+++ Website/FlaskApp/app.py
@@ -0,0 +1,22 @@
+"""
+file: app.py.py
+description:
+language: python3
+author: Anurag Kallurwar, ak6491@rit.edu
+"""
+
+import sys
+import matplotlib
+import sys
+
+
+def main():
+    """
+    The main function
+    :return: None
+    """
+    print("Hello")
+
+
+if __name__ == '__main__':
+    main()  # Calling Main Function
