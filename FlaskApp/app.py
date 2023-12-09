504fb6e434bfadd9a2ac8deae7d74e309a4b68e0
Anurag Kallurwar <anurag.kallurwar@gmail.com>
2023-12-08 20:03:30 -0500

    Deleted irrelevant

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -5,8 +5,8 @@ from matplotlib.figure import Figure
 import json
 from flask import Flask
 from flask import render_template, request, jsonify
-
 import code.simulate as s
+
 
 app = Flask(__name__)
 
@@ -14,37 +14,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
-
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello(name = None):
-    return render_template(
-        "hello.html",
-        name = name,
-        date = datetime.now()
-    )
-
-
-@app.route("/plot/")
-def plot():
-    # Generate the figure **without using pyplot**.
-    fig = Figure()
-    ax = fig.subplots()
-    ax.plot([1, 2])
-    # Save it to a temporary buffer.
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
-
-
-@app.route("/sim/")
-def sim():
-    # Generate the figure **without using pyplot**.
-    path = s.simulate()
-    return str(path)
 
 
 @app.route("/generate", methods=['POST'])
@@ -63,14 +32,3 @@ def genreate():
     result = [data, json.loads(obstacles), json.loads(simplified_edges), json.loads(path)]
     # print(result)
     return result
-
-
-@app.route("/test/")
-def test():
-    # Generate the figure **without using pyplot**.
-    return render_template("test.html")
-
-@app.route("/test2/")
-def test2():
-    # Generate the figure **without using pyplot**.
-    return render_template("test2.html")
